c4f0ab6dc46a51b2442ec5934f7ff021bf2f1f06
Tomilola-ng <tee.o2809@gmail.com>
2024-11-04 13:02:01 +0100

    TMP CHANGES

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
     This code is to automate some of the tasks in Google Sheets.
 """
 
+from typing import Union
+
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -15,7 +17,21 @@ credentials = Credentials.from_service_account_file("client_secret.json", scopes
 client = gspread.authorize(credentials)
 
 SHEET_ID = "1rfQZ5YxpF-_tGAtCKjczT2tYY3C1H5MRBxaEiU2-mUU"
-sheet = client.open_by_key(SHEET_ID)
+MAIN_SHEET = client.open_by_key(SHEET_ID)
 
-values_list = sheet.sheet1.row_values(1)
-print(values_list)
+def get_column(worksheet : gspread.worksheet, title : str, column_id : Union[int, list[int]]):
+    """
+        A Function that receives a title: string and column_id: number, worksheet: gspread.worksheet
+        It gets the column with the given column_id
+        IF YES It checks if the matching column's header is same as the given title
+        IF YES, return the rest of the column's values in an array starting from the second column
+        ELSE return error message "Column not found"
+    """
+    column = worksheet.col_values(column_id)
+    for index, value in enumerate(column):
+        if column[value] == title:
+            return column[index + 1:]
+        return "Column not found"
+
+ip_addresses = get_column(MAIN_SHEET.sheet1, "IP Address", 1)
+print(ip_addresses)
